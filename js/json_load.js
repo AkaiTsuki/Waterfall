e8a62d26ae5bf06f1a0c88bf210eed15c1bfac6b
Jiachi Liu <jiachiliu@Jiachis-Computer.local>
2013-04-06 13:30:24 -0400

    initial version
    loading from static json file

---
+++ js/json_load.js
@@ -0,0 +1,110 @@
+[
+	{
+		"url": "http://www.bluesdream.com/case/jquery/ublue-jquery-waterfall/images/img1.jpg",
+		"desc":"Image Description",
+		"author":"Author Information",
+		"height": "272"
+	},
+	{
+		"url": "http://www.bluesdream.com/case/jquery/ublue-jquery-waterfall/images/img2.jpg",
+		"desc":"Image Description",
+		"author":"Author Information",
+		"height": "299"
+	},
+	{
+		"url": "http://www.bluesdream.com/case/jquery/ublue-jquery-waterfall/images/img3.jpg",
+		"desc":"Image Description",
+		"author":"Author Information",
+		"height": "266"
+	},
+	{
+		"url": "http://www.bluesdream.com/case/jquery/ublue-jquery-waterfall/images/img4.jpg",
+		"desc":"Image Description",
+		"author":"Author Information",
+		"height": "266"
+	},
+	{
+		"url": "http://www.bluesdream.com/case/jquery/ublue-jquery-waterfall/images/img5.jpg",
+		"desc":"Image Description",
+		"author":"Author Information",
+		"height": "300"
+	},
+	{
+		"url": "http://www.bluesdream.com/case/jquery/ublue-jquery-waterfall/images/img6.jpg",
+		"desc":"Image Description",
+		"author":"Author Information",
+		"height": "284"
+	},
+	{
+		"url": "http://www.bluesdream.com/case/jquery/ublue-jquery-waterfall/images/img7.jpg",
+		"desc":"Image Description",
+		"author":"Author Information",
+		"height": "266"
+	},
+	{
+		"url": "http://www.bluesdream.com/case/jquery/ublue-jquery-waterfall/images/img8.jpg",
+		"desc":"Image Description",
+		"author":"Author Information",
+		"height": "251"
+	},
+	{
+		"url": "http://www.bluesdream.com/case/jquery/ublue-jquery-waterfall/images/img9.jpg",
+		"desc":"Image Description",
+		"author":"Author Information",
+		"height": "149"
+	},
+	{
+		"url": "http://www.bluesdream.com/case/jquery/ublue-jquery-waterfall/images/img10.jpg",
+		"desc":"Image Description",
+		"author":"Author Information",
+		"height": "845"
+	},
+	{
+		"url": "http://www.bluesdream.com/case/jquery/ublue-jquery-waterfall/images/img11.jpg",
+		"desc":"Image Description",
+		"author":"Author Information",
+		"height": "300"
+	},
+	{
+		"url": "http://www.bluesdream.com/case/jquery/ublue-jquery-waterfall/images/img12.jpg",
+		"desc":"Image Description",
+		"author":"Author Information",
+		"height": "122"
+	},
+	{
+		"url": "http://www.bluesdream.com/case/jquery/ublue-jquery-waterfall/images/img13.jpg",
+		"desc":"Image Description",
+		"author":"Author Information",
+		"height": "149"
+	},
+	{
+		"url": "http://www.bluesdream.com/case/jquery/ublue-jquery-waterfall/images/img14.jpg",
+		"desc":"Image Description",
+		"author":"Author Information",
+		"height": "300"
+	},
+	{
+		"url": "http://www.bluesdream.com/case/jquery/ublue-jquery-waterfall/images/img15.jpg",
+		"desc":"Image Description",
+		"author":"Author Information",
+		"height": "273"
+	},
+	{
+		"url": "http://www.bluesdream.com/case/jquery/ublue-jquery-waterfall/images/img16.jpg",
+		"desc":"Image Description",
+		"author":"Author Information",
+		"height": "304"
+	},
+	{
+		"url": "http://www.bluesdream.com/case/jquery/ublue-jquery-waterfall/images/img17.jpg",
+		"desc":"Image Description",
+		"author":"Author Information",
+		"height": "300"
+	},
+	{
+		"url": "http://www.bluesdream.com/case/jquery/ublue-jquery-waterfall/images/img18.jpg",
+		"desc":"Image Description",
+		"author":"Author Information",
+		"height": "301"
+	}
+]
